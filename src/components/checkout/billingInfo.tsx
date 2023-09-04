eab2870aa4912dacb32f24bdc7f423a4b95efa7e
gadsdencode <jordan.martens@osscontact.com>
2023-09-04 07:38:47 -0400

    UI tweaks/fixes

--- src/components/checkout/billingInfo.tsx
+++ src/components/checkout/billingInfo.tsx
@@ -9,7 +9,7 @@ export default function OrderSummary({
   return (
     <>
       <div className="form-check">
-        <input className="form-check-input" type="checkbox" value="" checked />
+        <input className="form-check-input" type="checkbox" value="true" defaultChecked={true} />
         <label className="custom-control-label">Same as shipping information</label>
       </div>
     </>
