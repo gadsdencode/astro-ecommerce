43bc12431f802212ef80ff0e00461692fbd03960
gadsdencode <jordan.martens@osscontact.com>
2023-09-04 08:46:57 -0400

    Image/verbiage updates

--- src/components/order/orderProductRow.tsx
+++ src/components/order/orderProductRow.tsx
@@ -22,7 +22,7 @@ export default function OrderSummary({
     <>
       <tr>
         <td scope="row" className="d-flex align-items-center text-sm text-body py-3">
-          <img className="w-20 w-lg-10 rounded-3" src="/path/to/new/image.jpg" alt={product.thumb_alt} />
+          <img className="w-20 w-lg-10 rounded-3" src="/images/oss-logo.png" alt={product.thumb_alt} />
           <p className="text-sm text-dark font-weight-bold mb-0 ms-3">{product.title}</p>
         </td>
         <td className="text-sm text-body align-middle pt-3 w-20">${product.price.toLocaleString()}</td>
