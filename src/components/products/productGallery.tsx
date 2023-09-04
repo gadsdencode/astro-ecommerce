57f194887bd35f4fd82e372395e1ce8ce57e3723
gadsdencode <jordan.martens@osscontact.com>
2023-09-04 08:04:59 -0400

    More image updates

--- src/components/products/productGallery.tsx
+++ src/components/products/productGallery.tsx
@@ -13,12 +13,12 @@ export default function ProductGallery({
     <>
       <div className="col-12 col-lg-6 d-flex">
         <div className="d-block">
-          <img className="w-90 max-height-150 mb-4 rounded-2" src={images[0].src} alt={images[0].alt} />
-          <img className="w-90 max-height-150 mb-4 rounded-2" src={images[1].src} alt={images[1].alt} />
-          <img className="w-90 max-height-150 mb-4 rounded-2" src={images[2].src} alt={images[2].alt} />
-          <img className="w-90 max-height-150 rounded-2" src={images[3].src} alt={images[3].alt} />
+          <img className="w-90 max-height-150 mb-4 rounded-2" src="/public/images/oss-logo.png" alt={images[0].alt} />
+          <img className="w-90 max-height-150 mb-4 rounded-2" src="/public/images/oss-logo.png" alt={images[1].alt} />
+          <img className="w-90 max-height-150 mb-4 rounded-2" src="/public/images/oss-logo.png" alt={images[2].alt} />
+          <img className="w-90 max-height-150 rounded-2" src="/public/images/oss-logo.png" alt={images[3].alt} />
         </div>
-        <img className="w-70 rounded-2" src={images[0].src} alt={images[0].alt} />
+        <img className="w-70 rounded-2" src="/public/images/oss-logo.png" alt={images[0].alt} />
       </div> 
     </>
   );
