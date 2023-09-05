d0f163c3b8688bc004f31102f8b510ed5bc6d3f6
gadsdencode <jordan.martens@osscontact.com>
2023-09-05 04:37:58 -0400

    Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-import react from "@astrojs/react";
-
-// https://astro.build/config
+import vercelServerless from '@astrojs/vercel/serverless';
+ 
 export default defineConfig({
-  integrations: [react()],
-  site: 'https://creativetimofficial.github.io',
-  base: '/astro-ecommerce'
+  output: 'server',
+  adapter: vercelServerless(),
 });
